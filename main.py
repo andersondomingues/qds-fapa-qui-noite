0469366f80ae9bb5fdc3fa79f13b2d9c74cb268c
Anderson Domingues <ti.andersondomingues@gmail.com>
2024-06-06 20:31:13 -0300

    adicionada função de divisão

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def soma(a: int, b: int) -> int:
     return a + b
 
-def sub(a: int, b:int) -> int:
+def sub(a: int, b: int) -> int:
     return a - b
 
-def mult(a: int, b:int) -> int:
+def mult(a: int, b: int) -> int:
     return a * b
+
+def div(a: int, b: int ) -> int:
+    if(b != 0):
+        return a / b
+    else: 
+        return None
