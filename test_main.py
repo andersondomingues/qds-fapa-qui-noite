32c8e93f35a9eb5a3982bf6f7b1b21c174543408
Anderson Domingues <ti.andersondomingues@gmail.com>
2024-06-06 20:25:21 -0300

    adicionado teste para subtração

--- test_main.py
+++ test_main.py
@@ -1,7 +1,10 @@
-from main import soma
+from main import soma, sub
 
 def test_soma():
     assert soma(1, 1) == 2
 
 def test_soma_2():
     assert soma(2, 3) == soma(3, 2)
+
+def test_soma_sub():
+    assert sub(3, 1) == soma(1, 1)
